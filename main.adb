d5ce1b78c3d2b558b2e502e54602fc80860dd6ba
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-27 18:37:56 +0200

    Add main.adb with initialization messages

---
+++ main.adb
@@ -0,0 +1,10 @@
+-- main.adb
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("============================================");
+   Put_Line (" Fortune's Algorithm (Voronoi) initialized.");
+   Put_Line (" Run 'make test' to execute V&V test suite.");
+   Put_Line ("============================================");
+end Main;
